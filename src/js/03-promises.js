ef9ba792272e37da454f726b371e5a874d403c80
Vycheslav1 <41229160+Vycheslav1@users.noreply.github.com>
2023-06-20 19:18:05 +0300

    Add files via upload

---
+++ src/js/03-promises.js
@@ -0,0 +1,52 @@
+
+let delayValue;
+
+function createPromise(position, stop) {
+
+  const shouldResolve = Math.random() > 0.3;
+
+  const promise = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+  if (shouldResolve) {
+    // Fulfill
+     resolve("Success! Value passed to resolve function");
+     
+        } else {
+          // Reject
+         reject("Error! Error passed to reject function");
+        }
+      }, stop);
+    });
+
+    promise.then(value => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(error => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  
+  }
+  
+
+function handleClick(evt){
+
+  console.clear();
+
+  evt.preventDefault();
+ 
+  const elements={delay,step,amount}=evt.currentTarget;
+  
+  for(let i=0;i<amount.value;i+=1)
+  {
+  
+   delayValue=i>0?delayValue+=Number.parseInt(step.value):Number.parseInt(delay.value); 
+
+  createPromise(i+1, delayValue);
+
+  }
+};
+const formData=document.querySelector(".form");
+
+formData.addEventListener("submit",handleClick);
